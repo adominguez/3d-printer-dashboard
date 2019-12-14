ab53d9ca09517d82d731617300aada217b237a29
ADominguez <dmartin.alberto@gmail.com>
2019-12-14 20:38:21 +0100

    feat(index): add authentication

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
+import authentication from './authentication';
 
 const baseURL = 'http://localhost:3128/';
 
 export const apiCall = (url, params, headers, method) => axios({
   method,
   url: baseURL + url,
-  params,
+  params: {
+    ...params,
+    authentication
+  },
   headers
 });
